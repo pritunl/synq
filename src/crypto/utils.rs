bbc1bda888a0b072f634ba36321439f7d8eaa2c4
Zachary Huff <zach.huff.386@gmail.com>
2026-01-03 17:07:06 -0500

    Add secret key to public key to crypto utils

--- src/crypto/utils.rs
+++ src/crypto/utils.rs
@@ -34,6 +34,22 @@ pub(super) fn decode_public_key(b64: &str) -> Result<PublicKey> {
     Ok(PublicKey::from(bytes))
 }
 
+pub fn secret_key_to_public_key(b64: &str) -> Result<String> {
+    let bytes = STANDARD_NO_PAD
+        .decode(b64)
+        .map_err(|e| Error::wrap(e, ErrorKind::Parse)
+        .with_msg("crypto: invalid base64 private key"))?;
+
+    let bytes: [u8; 32] = bytes
+        .try_into()
+        .map_err(|_| Error::new(ErrorKind::Invalid)
+        .with_msg("crypto: private key must be 32 bytes"))?;
+
+    let pub64 = STANDARD_NO_PAD.encode(SecretKey::from(bytes).public_key().to_bytes());
+
+    Ok(pub64)
+}
+
 pub fn generate_keypair() -> (String, String) {
     let secret_key = SecretKey::generate(&mut OsRng);
     let public_key = secret_key.public_key();
