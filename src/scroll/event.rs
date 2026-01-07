b4a819af994da2f9d042104c32109c9e29666b2f
Zachary Huff <zach.huff.386@gmail.com>
2026-01-07 11:02:14 -0500

    Add scroll event

---
+++ src/scroll/event.rs
@@ -0,0 +1,17 @@
+use super::constants::{ScrollSource};
+
+pub(crate) struct ScrollEvent {
+    pub source: ScrollSource,
+    pub delta_x: f64,
+    pub delta_y: f64,
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, Default)]
+pub(crate) struct InputEvent {
+    pub tv_sec: libc::time_t,
+    pub tv_usec: libc::suseconds_t,
+    pub type_: u16,
+    pub code: u16,
+    pub value: i32,
+}
