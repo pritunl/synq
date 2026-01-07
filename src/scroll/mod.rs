27d55228086b1fbab7ff6926bf2743c4d1094780
Zachary Huff <zach.huff.386@gmail.com>
2026-01-07 11:03:47 -0500

    Update scroll mod

--- src/scroll/mod.rs
+++ src/scroll/mod.rs
@@ -1,3 +1,12 @@
 #[allow(clippy::module_inception)]
 mod scroll;
-pub use scroll::*;
+mod event;
+mod utils;
+mod constants;
+pub use constants::*;
+mod blocker;
+pub use blocker::*;
+mod receiver;
+pub use receiver::*;
+mod sender;
+pub use sender::*;
