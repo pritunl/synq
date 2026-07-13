84765faa24810d195de5ffedb85a670a57798a57
Zachary Huff <zach.huff.386@gmail.com>
2026-07-13 17:29:45 -0400

    Remove port from broadcast constants

--- src/broadcast/constants.rs
+++ src/broadcast/constants.rs
@@ -1,4 +1,3 @@
-pub(crate) const BROADCAST_PORT: u16 = 39120;
 pub(crate) const BROADCAST_PREFIX: &str = "synq";
 pub(crate) const BROADCAST_INTERVAL: u64 = 1000;
 pub(crate) const EXCHANGE_TIMEOUT: u64 = 5000;
